aa70985ed70444fd1efb5b129b3796c69be93798
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-05-08 03:14:06 -0300

    add toasts and contexts

---
+++ web/src/components/ToastContainer/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+
+import { Container, Toast } from './styles';
+
+import { ToastMessage, useToast } from '../../hooks/toast';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const { removeToast } = useToast();
+
+  return (
+    <Container>
+      {messages.map((message) => (
+        <Toast
+          key={message.id}
+          type={message.type}
+          hasDescription={!!message.description}
+        >
+          <FiAlertCircle size={20} />
+
+          <div>
+            <strong>{message.title}</strong>
+            {message.description && <p>{message.description}</p>}
+
+            <button type="button" onClick={() => removeToast(message.id)}>
+              <FiXCircle size={18} />
+            </button>
+          </div>
+        </Toast>
+      ))}
+    </Container>
+  );
+};
+
+export default ToastContainer;
