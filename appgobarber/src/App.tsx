a8934fa9f95a4c02b730c84464f0e1ac78bcfa39
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-05-09 22:26:24 -0300

    linting and routing ok

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,37 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <View style={styles.container}>
-    <Image
-      source={{
-        uri:
-          'https://i.pinimg.com/originals/45/32/f9/4532f92e4e002bb591ede7ce7ff95e15.png',
-      }}
-      style={{
-        height: 150,
-        width: 150,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-      }}
-    />
-    <Text style={styles.texto}>Je mange une orange</Text>
-  </View>
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'orange',
-  },
-
-  texto: {
-    fontSize: 22,
-    color: '#fff',
-  },
-});
 
 export default App;
