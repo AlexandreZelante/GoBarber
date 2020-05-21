5fbdecbb9a73faad4dc002c8be2ca654e18e8af2
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-05-21 03:29:34 -0300

    adding tests with jest

---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not ble able to create a new user with same email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
