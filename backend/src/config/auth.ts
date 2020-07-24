25e86089dab685c7b3ce9eee20360a5215eca1b9
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-07-23 22:16:17 -0300

    add

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'eb1baa44fa3cf99f3c8cd261cb898964',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -15,7 +16,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
