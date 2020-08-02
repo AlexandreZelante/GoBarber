45349cead9e6fe8f82543418516dfd528ba678d1
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-08-02 01:08:25 -0300

    add

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
       <Header>
@@ -14,20 +30,105 @@ const Dashboard: React.FC = () => {
 
           <Profile>
             <img
-              src="https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4"
-              alt="Alexandre Zelante"
+              src={
+                user.avatar_url
+                  ? user.avatar_url
+                  : 'https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4'
+              }
+              alt={user.name}
             />
             <div>
               <span>Bem-vindo,</span>
-              <strong>Alexandre Zelante</strong>
+              <strong>{user.name}</strong>
             </div>
           </Profile>
 
-          <button type="button">
+          <button type="button" onClick={signOut}>
             <FiPower />
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4"
+                alt=""
+              />
+
+              <strong>Diego Fernandes</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4"
+                  alt=""
+                />
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4"
+                  alt=""
+                />
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/40640392?s=460&u=ff5a56d5c9207f8d22080814dcd074442d9f70e4&v=4"
+                  alt=""
+                />
+                <strong>Diego Fernandes</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
