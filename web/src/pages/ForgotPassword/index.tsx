ab36012946f030f1f22ce910e154cd96a5a8b067
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-08-03 01:14:06 -0300

    add

--- web/src/pages/ForgotPassword/index.tsx
+++ web/src/pages/ForgotPassword/index.tsx
@@ -102,7 +102,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
